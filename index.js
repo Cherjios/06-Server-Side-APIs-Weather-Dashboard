0e86ccd99becb0a0d0f5488b168a8f46fc2cf047
cherjios <slopez0286@gmail.com>
2020-06-28 23:09:20 -0700

    Fixing rendering function

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var cityList =$("#city-list");
 
-
 var cities = [];
+console.log(cities);
 
 //Calling function init();
 init();
-
-
-
 
 //Function init();
 function init(){
@@ -21,24 +18,27 @@ function init(){
       }
     // Render cities to the DOM
     renderCities();
+    // console.log(cities);
 }
 
 //Function StoreCities()
 function storeCities(){
    // Stringify and set "cities" key in localStorage to cities array
   localStorage.setItem("cities", JSON.stringify(cities));
+  console.log(localStorage);
 }
 
 //Function renderCities()
 function renderCities() {
-    // Clear todoList element and update todoCountSpan
-    cityList.text = "";
-
+    // Clear cityList element
+    // cityList.text = "";
+    // cityList.HTML = "";
+    cityList.empty();
+    
     // Render a new li for each city
     for (var i = 0; i < cities.length; i++) {
       var city = cities[i];
-      console.log(city);
-  
+      
       var li = $("<li>").text(city);
       li.attr("data-index", i);
       li.attr("class", "list-group-item");
@@ -54,10 +54,20 @@ function renderCities() {
 
     // This line will grab the city from the input box
     var city = $("#city-input").val().trim();
-
+    
+    // Return from function early if submitted city is blank
+    if (city === "") {
+        return;
+    }
     //Adding city-input to the city array
     cities.push(city);
-    renderCities();
+    // console.log(cities);
+    // city= "";
+    console.log(cities);
+    console.log(city);
+    // Store updated cities in localStorage, re-render the list
+  storeCities();
+  renderCities();
 
   });
 
